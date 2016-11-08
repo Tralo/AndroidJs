1f8ee80f4c037598d69e024bff8b239d0e628795
Tralo <13580548169@163.com>
2016-11-08 16:38:04 +0800

    添加资源，初始化工具

--- app/src/main/java/com/example/androidjs/MainActivity.java
+++ app/src/main/java/com/example/androidjs/MainActivity.java
@@ -2,12 +2,55 @@ package com.example.androidjs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnJavaAndJs;
+    private Button btnJsCallJava;
+    private Button btnJsCallPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
     }
+
+    /**
+     * Find the Views in the layout<br />
+     * <br />
+     * Auto-created on 2016-11-08 16:33:59 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    private void findViews() {
+        btnJavaAndJs = (Button)findViewById( R.id.btn_java_and_js );
+        btnJsCallJava = (Button)findViewById( R.id.btn_js_call_java );
+        btnJsCallPhone = (Button)findViewById( R.id.btn_js_call_phone );
+
+        btnJavaAndJs.setOnClickListener(this);
+        btnJsCallJava.setOnClickListener(this);
+        btnJsCallPhone.setOnClickListener(this);
+    }
+
+    /**
+     * Handle button click events<br />
+     * <br />
+     * Auto-created on 2016-11-08 16:33:59 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    @Override
+    public void onClick(View v) {
+        if ( v == btnJavaAndJs ) {
+            // Handle clicks for btnJavaAndJs
+        } else if ( v == btnJsCallJava ) {
+            // Handle clicks for btnJsCallJava
+        } else if ( v == btnJsCallPhone ) {
+            // Handle clicks for btnJsCallPhone
+        }
+    }
+
+
+
 }
